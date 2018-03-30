64570cc96779fa0b4eee6baacf67a64a9457e4c3
Sindyukov Alexei <rodjer200586@gmail.com>
2018-03-30 19:29:50 +0400

    Update frontend

--- frontend/e2e/app.e2e-spec.ts
+++ frontend/e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('frontend App', () => {
+describe('minelixir App', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -9,6 +9,6 @@ describe('frontend App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Welcome to mine!');
   });
 });
