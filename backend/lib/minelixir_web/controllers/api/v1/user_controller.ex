d8cf9c0c82d37340506f6e4a97a705028ee75471
Sindyukov Alexei <rodjer200586@gmail.com>
2018-03-30 20:42:34 +0400

    Update users api

--- backend/lib/minelixir_web/controllers/api/v1/user_controller.ex
+++ backend/lib/minelixir_web/controllers/api/v1/user_controller.ex
@@ -1,17 +1,15 @@
 defmodule MinelixirWeb.Controllers.Api.V1.UserController do
   use MinelixirWeb, :controller
+  alias Minelixir.Tools.User
 
-  def index(conn, _params) do
-    users = File.read(
-      Application.get_env(:minelixir, :minecraft_server_path) <> "/usercache.json"
-    )
-    case users do
+  def list(conn, _params) do
+    list = User.list()
+    case list do
       {:ok, data} ->
         conn
         |> put_status(200)
         |> json(
              data
-             |> Poison.decode!()
            )
       _ ->
         conn
@@ -24,20 +22,16 @@ defmodule MinelixirWeb.Controllers.Api.V1.UserController do
     end
   end
 
-  def detail(conn, params) do
+  def stats(conn, params) do
     uuid = params
            |> Map.get("uuid")
-    stats = File.read(
-              Application.get_env(:minelixir, :minecraft_server_path) <> "/world/stats/" <> uuid <> ".json"
-            )
-            |> IO.inspect()
+    stats = User.stats(uuid)
     case stats do
       {:ok, data} ->
         conn
         |> put_status(200)
         |> json(
              data
-             |> Poison.decode!()
            )
       _ ->
         conn
@@ -49,4 +43,27 @@ defmodule MinelixirWeb.Controllers.Api.V1.UserController do
            )
     end
   end
+
+  def advancements(conn, params) do
+    uuid = params
+           |> Map.get("uuid")
+    advancements = User.advancements(uuid)
+    case advancements do
+      {:ok, data} ->
+        conn
+        |> put_status(200)
+        |> json(
+             data
+           )
+      _ ->
+        conn
+        |> put_status(400)
+        |> json(
+             %{
+
+             }
+           )
+    end
+  end
+
 end
